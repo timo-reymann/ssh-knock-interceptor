c67474d7d5a5f23d3696f510cb0e72c50fb4e427
Timo Reymann <Timo.Reymann@trustedshops.de>
2022-09-18 14:21:37 +0200

    feat: Make port knocker entrypoint compact
    Allow 'old integration as well', but simplify the config required

--- knock/PortKnocker.py
+++ knock/PortKnocker.py
@@ -3,24 +3,28 @@ import select
 import time
 from config import KnockConfigEntry
 
+
+def print_env(var: str, val: str) -> str:
+    print(f"export {var}='{val}';")
+
+
 class PortKnocker:
     def __init__(self, config_entry: KnockConfigEntry):
         self.config_entry = config_entry
-        self.address_family, _, _, _, ip = socket.getaddrinfo(
+        self.address_family, _, _, _, _ = socket.getaddrinfo(
             host=self.config_entry.knock_host,
             port=None,
             flags=socket.AI_ADDRCONFIG
         )[0]
-        self.ip_address = ip[0]
-
-    def print_env(self, var: str, val: str) -> str:
-        print(f"export {var}='{val}';")
 
     def knock(self):
         ports = self.config_entry.sequence.split(" ")
         last_index = len(ports) - 1
         for i, port in enumerate(ports):
+            # global use udp setting
             use_udp = self.config_entry.use_udp == "yes"
+
+            # allow custom protocol in port spec
             if port.find(':') != -1:
                 port, protocol = port.split(':', 2)
                 if protocol == 'tcp':
@@ -28,25 +32,21 @@ class PortKnocker:
                 elif protocol == 'udp':
                     use_udp = True
                 else:
-                    error = 'Invalid protocol "{}" given. Allowed values are "tcp" and "udp".'
-                    raise ValueError(error.format(protocol))
-
-
-            # print('hitting %s %s:%d' % ('udp' if use_udp else 'tcp', self.config_entry.knock_host, int(port)))
+                    raise ValueError(
+                        'Invalid protocol "{}" given. Allowed values are "tcp" and "udp".'.format(protocol)
+                    )
 
             s = socket.socket(self.address_family, socket.SOCK_DGRAM if use_udp else socket.SOCK_STREAM)
             s.setblocking(False)
-
             socket_address = (self.config_entry.knock_host, int(port))
             if use_udp:
                 s.sendto(b'', socket_address)
             else:
                 s.connect_ex(socket_address)
                 select.select([s], [s], [s], 1)
-
             s.close()
 
             if self.config_entry.duration and i != last_index:
                 time.sleep(self.config_entry.duration / 1000)
 
-        self.print_env("KNOCK_HOST", self.config_entry.knock_host)
+        print_env("KNOCK_HOST", self.config_entry.knock_host)
